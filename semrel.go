6b47ee8c1c91171f5e91b15987f72371b0d6fa0d
Juhani Ränkimies <juhani@juranki.com>
2018-04-03 22:27:10 +0300

    feat: implement framework for processing release data

---
+++ semrel.go
@@ -0,0 +1,97 @@
+package semrel
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/blang/semver"
+)
+
+// BumpLevel of the release and/or individual commit
+type BumpLevel int
+
+// BumpLevel values
+const (
+	NoBump    BumpLevel = iota
+	BumpPatch           = iota
+	BumpMinor           = iota
+	BumpMajor           = iota
+)
+
+// ChangeCategory is used to group
+type ChangeCategory string
+
+// ReleaseInput contains data collected from VCS and an analyzer function
+type ReleaseInput struct {
+	CurrentVersion    semver.Version
+	UnreleasedChanges []RawChange
+	ChangeAnalyzer    func(Message string) ([]Change, error)
+}
+
+// RawChange provides accessors to VCS commit data
+type RawChange interface {
+	Message() string
+	SHA() string
+	Time() time.Time
+}
+
+// Change captures ChangeAnalyzer results
+type Change interface {
+	Category() string
+	BumpLevel() BumpLevel
+	Render() string
+}
+
+// ReleaseOutput contains information for next release
+type ReleaseOutput struct {
+	CurrentVersion semver.Version
+	NextVersion    semver.Version
+	Changes        map[string][]Change
+}
+
+// Release processes the release data
+func Release(input *ReleaseInput) (*ReleaseOutput, error) {
+	output := &ReleaseOutput{
+		CurrentVersion: input.CurrentVersion,
+		NextVersion:    input.CurrentVersion,
+		Changes:        map[string][]Change{},
+	}
+	bumpLevel := NoBump
+	for _, rawChange := range input.UnreleasedChanges {
+		changes, err := input.ChangeAnalyzer(rawChange.Message())
+		if err != nil {
+			return nil, err
+		}
+		for _, change := range changes {
+			if category, catOK := output.Changes[change.Category()]; catOK {
+				output.Changes[change.Category()] = append(category, change)
+			} else {
+				output.Changes[change.Category()] = []Change{change}
+			}
+			if change.BumpLevel() > bumpLevel {
+				bumpLevel = change.BumpLevel()
+			}
+		}
+	}
+	output.NextVersion = bump(output.CurrentVersion, bumpLevel)
+	return output, nil
+}
+
+func bump(curr semver.Version, bumpLevel BumpLevel) semver.Version {
+	var major uint64
+	var minor uint64
+	var patch uint64
+	if bumpLevel == BumpMajor && curr.Major > 0 {
+		major = curr.Major + 1
+	}
+	if bumpLevel == BumpMinor || (curr.Major == 0 && bumpLevel == BumpMajor) {
+		major = curr.Major
+		minor = curr.Minor + 1
+	}
+	if bumpLevel == BumpPatch {
+		major = curr.Major
+		minor = curr.Minor
+		patch = curr.Patch + 1
+	}
+	return semver.MustParse(fmt.Sprintf("%d.%d.%d", major, minor, patch))
+}
---
+++ semrel_test.go
@@ -0,0 +1,120 @@
+package semrel
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/blang/semver"
+)
+
+type dummyCommit string
+
+func (commit dummyCommit) Message() string { return string(commit) }
+func (commit dummyCommit) SHA() string     { return string(commit) }
+func (commit dummyCommit) Time() time.Time { return time.Now() }
+
+func (change BumpLevel) Category() string     { return fmt.Sprintf("%d", int(change)) }
+func (change BumpLevel) BumpLevel() BumpLevel { return change }
+func (change BumpLevel) Render() string       { return "" }
+
+func dummyAnalyzer(msg string) ([]Change, error) {
+	if strings.HasPrefix(msg, "fix") {
+		return []Change{BumpLevel(BumpPatch)}, nil
+	}
+	if strings.HasPrefix(msg, "feat") {
+		return []Change{BumpLevel(BumpMinor)}, nil
+	}
+	if strings.HasPrefix(msg, "break") {
+		return []Change{BumpLevel(BumpMajor)}, nil
+	}
+	if strings.HasPrefix(msg, "fail") {
+		return nil, fmt.Errorf("fail")
+	}
+	return []Change{}, nil
+}
+
+func TestBump(t *testing.T) {
+	if bump(semver.MustParse("0.0.0"), NoBump).String() != "0.0.0" {
+		t.Error("NoBump")
+	}
+	if bump(semver.MustParse("0.0.0"), BumpPatch).String() != "0.0.1" {
+		t.Error("BumpPatch")
+	}
+	if bump(semver.MustParse("0.2.1"), BumpMinor).String() != "0.3.0" {
+		t.Error("BumpMinor")
+	}
+	if bump(semver.MustParse("0.2.1"), BumpMajor).String() != "0.3.0" {
+		t.Error("BumpMajor alpha")
+	}
+	if bump(semver.MustParse("1.2.1"), BumpMajor).String() != "2.0.0" {
+		t.Error("BumpMajor")
+	}
+}
+
+func TestRelease1(t *testing.T) {
+	input := &ReleaseInput{
+		CurrentVersion: semver.MustParse("0.0.0"),
+		UnreleasedChanges: []RawChange{
+			dummyCommit("fix"),
+		},
+		ChangeAnalyzer: dummyAnalyzer,
+	}
+	output, err := Release(input)
+	if err != nil {
+		t.Error(err)
+	}
+	if output.NextVersion.String() != "0.0.1" {
+		t.Errorf("expected 0.0.1, got %s", output.NextVersion.String())
+	}
+	if len(output.Changes["1"]) != 1 {
+		t.Errorf("expected one fix, got %d", len(output.Changes["1"]))
+	}
+}
+
+func TestRelease2(t *testing.T) {
+	input := &ReleaseInput{
+		CurrentVersion: semver.MustParse("1.2.3"),
+		UnreleasedChanges: []RawChange{
+			dummyCommit("fix"),
+			dummyCommit("fix"),
+			dummyCommit("feat"),
+			dummyCommit("break"),
+		},
+		ChangeAnalyzer: dummyAnalyzer,
+	}
+	output, err := Release(input)
+	if err != nil {
+		t.Error(err)
+	}
+	if output.NextVersion.String() != "2.0.0" {
+		t.Errorf("expected 0.0.1, got %s", output.NextVersion.String())
+	}
+	if len(output.Changes["1"]) != 2 {
+		t.Errorf("expected two fixes, got %d", len(output.Changes["1"]))
+	}
+	if len(output.Changes["2"]) != 1 {
+		t.Errorf("expected one feature, got %d", len(output.Changes["2"]))
+	}
+	if len(output.Changes["3"]) != 1 {
+		t.Errorf("expected one breaking change, got %d", len(output.Changes["3"]))
+	}
+}
+
+func TestRelease3(t *testing.T) {
+	input := &ReleaseInput{
+		CurrentVersion: semver.MustParse("1.2.3"),
+		UnreleasedChanges: []RawChange{
+			dummyCommit("fix"),
+			dummyCommit("fix"),
+			dummyCommit("fail"),
+			dummyCommit("break"),
+		},
+		ChangeAnalyzer: dummyAnalyzer,
+	}
+	_, err := Release(input)
+	if err == nil || err.Error() != "fail" {
+		t.Errorf("expected error, got %+v", err)
+	}
+}
