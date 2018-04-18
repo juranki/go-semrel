40c3c3e2135e1a05e78d790f30580e9a7e662b8c
Juhani Ränkimies <juhani@juranki.com>
2018-04-18 18:48:13 +0300

    fix: fix helper functions and  assert messages in inspectgit tests

--- inspectgit/inspectgit_test.go
+++ inspectgit/inspectgit_test.go
@@ -13,19 +13,21 @@ import (
 	"gopkg.in/src-d/go-git.v4/storage/memory"
 )
 
-func setupRepo() (*git.Repository, *git.Worktree) {
+func setupRepo(t *testing.T) (*git.Repository, *git.Worktree) {
+	t.Helper()
 	r, err := git.Init(memory.NewStorage(), memfs.New())
 	if err != nil {
-		log.Fatalln(err)
+		t.Fatal(err)
 	}
 	w, err := r.Worktree()
 	if err != nil {
-		log.Fatalln(err)
+		t.Fatal(err)
 	}
 	return r, w
 }
 
-func commit(w *git.Worktree, msg string) plumbing.Hash {
+func commit(t *testing.T, w *git.Worktree, msg string) plumbing.Hash {
+	t.Helper()
 	hash, err := w.Commit(msg, &git.CommitOptions{
 		Author: &object.Signature{
 			Name:  "a",
@@ -33,13 +35,13 @@ func commit(w *git.Worktree, msg string) plumbing.Hash {
 			When:  time.Now(),
 		},
 	})
-	// fmt.Println(msg, hash.String())
 	if err != nil {
-		log.Fatal(err)
+		t.Fatal(err)
 	}
 	return hash
 }
-func merge(w *git.Worktree, msg string, parents []plumbing.Hash) plumbing.Hash {
+func merge(t *testing.T, w *git.Worktree, msg string, parents []plumbing.Hash) plumbing.Hash {
+	t.Helper()
 	hash, err := w.Commit(msg, &git.CommitOptions{
 		Author: &object.Signature{
 			Name:  "a",
@@ -48,47 +50,48 @@ func merge(w *git.Worktree, msg string, parents []plumbing.Hash) plumbing.Hash {
 		},
 		Parents: parents,
 	})
-	// fmt.Println(msg, hash.String())
 	if err != nil {
-		log.Fatal(err)
+		t.Fatal(err)
 	}
 	return hash
 }
 
-func tag(r *git.Repository, hash plumbing.Hash, version string) {
+func tag(t *testing.T, r *git.Repository, hash plumbing.Hash, version string) {
+	t.Helper()
 	n := plumbing.ReferenceName(fmt.Sprintf("refs/tags/v%s", version))
 	tag := plumbing.NewHashReference(n, hash)
 	err := r.Storer.SetReference(tag)
 	if err != nil {
-		log.Fatal(err)
+		t.Fatal(err)
 	}
 }
 
 func checkReleaseData(t *testing.T, r *git.Repository, n int, version string) {
+	t.Helper()
 	vs, err := getVersions(r)
 	if err != nil {
 		t.Error(err)
 	}
-	v, cs, err := getUnreleasedCommits(r, vs)
+	vcsData, err := getUnreleasedCommits(r, vs)
 	if err != nil {
 		log.Fatal(err)
 	}
-	if len(cs) != n {
-		fmt.Printf("commits: %+v\n", cs)
-		fmt.Printf("versions: %+v\n", vs)
-		fmt.Printf("version: %+v\n", v)
-		t.Errorf("expected %d commits, got %d", n, len(cs))
+	if len(vcsData.UnreleasedCommits) != n {
+		t.Logf("commits: %+v\n", vcsData.UnreleasedCommits)
+		t.Logf("versions: %+v\n", vs)
+		t.Logf("version: %+v\n", vcsData.CurrentVersion)
+		t.Errorf("got %d commits, want %d", len(vcsData.UnreleasedCommits), n)
 	}
-	if v.String() != version {
-		fmt.Printf("commits: %+v\n", cs)
-		fmt.Printf("versions: %+v\n", vs)
-		fmt.Printf("version: %+v\n", v)
-		t.Errorf("expected %s, got %s", version, v.String())
+	if vcsData.CurrentVersion.String() != version {
+		t.Logf("commits: %+v\n", vcsData.UnreleasedCommits)
+		t.Logf("versions: %+v\n", vs)
+		t.Logf("version: %+v\n", vcsData.CurrentVersion)
+		t.Errorf("got %s, want %s", vcsData.CurrentVersion.String(), version)
 	}
 }
 
 func TestGetVersions(t *testing.T) {
-	r, w := setupRepo()
+	r, w := setupRepo(t)
 
 	checkVersionCount := func(n int) {
 		vs, err := getVersions(r)
@@ -96,57 +99,57 @@ func TestGetVersions(t *testing.T) {
 			t.Error(err)
 		}
 		if len(vs) != n {
-			t.Errorf("expected %d versions, got %d", n, len(vs))
+			t.Errorf("got %d versions, want %d", len(vs), n)
 		}
 	}
 
 	checkVersionCount(0)
 
-	hash := commit(w, "initial")
-	tag(r, hash, "2.3.4")
+	hash := commit(t, w, "initial")
+	tag(t, r, hash, "2.3.4")
 
 	checkVersionCount(1)
 }
 
 func TestGetUnreleasedCommits(t *testing.T) {
-	r, w := setupRepo()
+	r, w := setupRepo(t)
 
-	commit(w, "initial")
+	commit(t, w, "initial")
 	checkReleaseData(t, r, 1, "0.0.0")
-	hash := commit(w, "1")
+	hash := commit(t, w, "1")
 	checkReleaseData(t, r, 2, "0.0.0")
-	tag(r, hash, "1.0.0")
+	tag(t, r, hash, "1.0.0")
 	checkReleaseData(t, r, 0, "1.0.0")
-	commit(w, "2")
+	commit(t, w, "2")
 	checkReleaseData(t, r, 1, "1.0.0")
 }
 
 func TestMultipleTagsOnCommit(t *testing.T) {
-	r, w := setupRepo()
+	r, w := setupRepo(t)
 
-	commit(w, "initial")
+	commit(t, w, "initial")
 	checkReleaseData(t, r, 1, "0.0.0")
-	hash := commit(w, "1")
+	hash := commit(t, w, "1")
 	checkReleaseData(t, r, 2, "0.0.0")
-	tag(r, hash, "1.0.0")
-	tag(r, hash, "1.0.1")
+	tag(t, r, hash, "1.0.0")
+	tag(t, r, hash, "1.0.1")
 	checkReleaseData(t, r, 0, "1.0.1")
-	hash = commit(w, "2")
-	tag(r, hash, "2.0.0")
-	tag(r, hash, "2.0.1")
+	hash = commit(t, w, "2")
+	tag(t, r, hash, "2.0.0")
+	tag(t, r, hash, "2.0.1")
 	checkReleaseData(t, r, 0, "2.0.1")
 }
 
 func TestMerge(t *testing.T) {
-	r, w := setupRepo()
+	r, w := setupRepo(t)
 
-	commit(w, "initial")
-	a1 := commit(w, "a1")
-	a2 := commit(w, "a2")
+	commit(t, w, "initial")
+	a1 := commit(t, w, "a1")
+	a2 := commit(t, w, "a2")
 	checkReleaseData(t, r, 3, "0.0.0")
-	tag(r, a2, "1.0.0")
+	tag(t, r, a2, "1.0.0")
 	checkReleaseData(t, r, 0, "1.0.0")
-	a3 := commit(w, "a3")
+	a3 := commit(t, w, "a3")
 	checkReleaseData(t, r, 1, "1.0.0")
 
 	err := w.Checkout(&git.CheckoutOptions{
@@ -166,12 +169,12 @@ func TestMerge(t *testing.T) {
 	f.Write([]byte("hello"))
 	f.Close()
 	w.Add("foo")
-	commit(w, "b1")
+	commit(t, w, "b1")
 	// fmt.Printf("%+v\n", head)
 	checkReleaseData(t, r, 3, "0.0.0")
-	commit(w, "b2")
-	b3 := commit(w, "b3")
+	commit(t, w, "b2")
+	b3 := commit(t, w, "b3")
 	checkReleaseData(t, r, 5, "0.0.0")
-	merge(w, "merge", []plumbing.Hash{b3, a3})
+	merge(t, w, "merge", []plumbing.Hash{b3, a3})
 	checkReleaseData(t, r, 5, "1.0.0")
 }
